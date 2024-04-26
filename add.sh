4d8977a147b8f834374b9873a6f9113d0b6d9f54
Paul Naber <paulnaber@Pauls-MBP-3.inside-m2m.de>
2024-04-26 08:46:46 +0200

    add delta to git and lazygit configs

--- add.sh
+++ add.sh
@@ -1,27 +1,12 @@
 #! /bin/bash
 cp ~/Desktop/Profiles.json ~/Documents/Projects/configs/terminal/iTerm2/ 
-
-
 cp ~/Library/Application\ Support/Code/User/settings.json ~/Documents/Projects/configs/vscode
-
-
 cp ~/Desktop/Keybindings.itermkeymap ~/Documents/Projects/configs/terminal/iTerm2/
-
-
 cp ~/.tmux.conf ~/Documents/Projects/configs/terminal
-
-
 cp ~/.zshrc ~/Documents/Projects/configs/terminal
-
-
 cp ~/Library/Application\ Support/Code/User/settings.json ~/Documents/Projects/configs/vscode
-
-
 cp ~/Library/Application\ Support/Code/User/keybindings.json ~/Documents/Projects/configs/vscode
-
-
 cp ~/.p10k.zsh ~/Documents/Projects/configs/terminal
-
 cp -R ~/.config/nvim ~/Documents/Projects/configs
-
 cp ~/.gitconfig ~/Documents/Projects/configs/git/
+ cp ~/Library/Application\ Support/lazygit/config.yml ~/Documents/Projects/configs/git/lazygit/
