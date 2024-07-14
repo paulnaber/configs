bda7d37d806a9e1ba2e6c59e4be6ba146c617270
Paul Naber <paul.naber@inside-m2m.de>
2024-07-14 13:21:43 +0200

    update add script to now ask for what to add, add new vscode settings and vimium-options

--- add.sh
+++ add.sh
@@ -1,13 +1,33 @@
-#! /bin/bash
-cp ~/Desktop/Profiles.json ~/Documents/Projects/configs/terminal/iTerm2/ 
-cp ~/Library/Application\ Support/Code/User/settings.json ~/Documents/Projects/configs/vscode
-cp ~/Desktop/Keybindings.itermkeymap ~/Documents/Projects/configs/terminal/iTerm2/
-cp ~/.tmux.conf ~/Documents/Projects/configs/terminal
-cp ~/.zshrc ~/Documents/Projects/configs/terminal
-cp ~/Library/Application\ Support/Code/User/settings.json ~/Documents/Projects/configs/vscode
-cp ~/Library/Application\ Support/Code/User/keybindings.json ~/Documents/Projects/configs/vscode
-cp ~/.p10k.zsh ~/Documents/Projects/configs/terminal
-cp -R ~/.config/nvim ~/Documents/Projects/configs
-cp ~/.gitconfig ~/Documents/Projects/configs/git/
-cp ~/Library/Application\ Support/lazygit/config.yml ~/Documents/Projects/configs/git/lazygit/
-cp ~/Downloads/vimium-options.json ~/Documents/Projects/configs/vimium-options.json
+#!/bin/bash
+
+# Function to prompt user and copy file if yes
+prompt_and_copy() {
+    local src="$1"
+    local dest="$2"
+    local default="y"
+
+    read -p "Do you want to copy $src to $dest? [Y/n]: " -n 1 -r
+    echo    # Move to a new line
+
+    if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
+        if cp -R "$src" "$dest"; then
+            echo "Copied $src to $dest"
+        else
+            echo "Failed to copy $src to $dest"
+        fi
+    else
+        echo "Skipped $src"
+    fi
+}
+
+prompt_and_copy "$HOME/Library/Application Support/Code/User/settings.json" "$HOME/Documents/Projects/configs/vscode"
+prompt_and_copy "$HOME/Library/Application Support/Code/User/keybindings.json" "$HOME/Documents/Projects/configs/vscode"
+prompt_and_copy "$HOME/Downloads/vimium-options.json" "$HOME/Documents/Projects/configs/vimium-options.json"
+prompt_and_copy "$HOME/.zshrc" "$HOME/Documents/Projects/configs/terminal"
+prompt_and_copy "$HOME/Desktop/Profiles.json" "$HOME/Documents/Projects/configs/terminal/iTerm2/"
+prompt_and_copy "$HOME/Desktop/Keybindings.itermkeymap" "$HOME/Documents/Projects/configs/terminal/iTerm2/"
+prompt_and_copy "$HOME/.tmux.conf" "$HOME/Documents/Projects/configs/terminal"
+prompt_and_copy "$HOME/.p10k.zsh" "$HOME/Documents/Projects/configs/terminal"
+prompt_and_copy "$HOME/.config/nvim" "$HOME/Documents/Projects/configs"
+prompt_and_copy "$HOME/.gitconfig" "$HOME/Documents/Projects/configs/git/"
+prompt_and_copy "$HOME/Library/Application Support/lazygit/config.yml" "$HOME/Documents/Projects/configs/git/lazygit/"
